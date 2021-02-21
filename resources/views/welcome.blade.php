0fbedb783f50e3dbc2b68f91a01634315adecd3b
phlimasg <42648475+phlimasg@users.noreply.github.com>
2021-02-21 12:44:42 -0300

    make it better

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    GIT
                 </div>
 
                 <div class="links">
