9e0172bad055462780edb3b2c93975d7a95b6d3d
phlimasg <42648475+phlimasg@users.noreply.github.com>
2021-02-27 14:48:38 -0300

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->string('profile_picture')->nullable();
             $table->string('service')->nullable();            
             $table->string('token')->nullable();            
